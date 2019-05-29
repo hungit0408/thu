3c8fe7a2795ddfecf99ee346728bc908278adb29
DAO VAN <dvhung@fresher.local>
2019-05-29 18:13:08 +0700

    new

---
+++ DemoQLTV/MISA.QLTV.VN/MISA.QLTV.VN/scripts/common/common.js
@@ -0,0 +1,100 @@
+﻿$(document).ready(function () {
+    $('.dropdown-trigger').on('mouseover', function () {
+        $(this).children('.dropdown-content').show();
+    });
+    $('.dropdown-trigger').on('mouseleave', function () {
+        $(this).children('.dropdown-content').hide();
+    });
+});
+
+class Menu {
+
+    constructor() {
+
+    }
+
+
+    //adasdad
+     //created by XXX(24/05/2019
+    //showMenu() {
+
+    //    $('.lbsetting').mouseleave(function () {
+
+    //        $(this).children('.hide-header-menu').hide();
+    //    });
+
+    //    $('.lbsetting').mouseover(function () {
+
+    //        $(this).children('.hide-header-menu').show();
+    //    });
+    //}
+
+    //showluuthong() {
+
+
+
+    //    $('.lbcirculate').mouseleave(function () {
+
+    //        $(this).children('.hide-header-menu-circulate1').hide();
+
+
+    //    });
+
+    //    $('.lbcirculate').mouseover(function () {
+
+    //        $(this).children('.hide-header-menu-circulate1').show();
+
+
+    //    });
+
+    //}
+    //showdanhmuc() {
+
+
+
+    //    $('.danhmuc').mouseleave(function () {
+
+    //        $(this).children('.hide-header-menu').hide();
+
+
+    //    });
+
+    //    $('.danhmuc').mouseover(function () {
+
+    //        $(this).children('.hide-header-menu').show();
+
+
+    //    });
+
+    //}
+    //showbook() {
+
+    //    $('.lbnewspaper').mouseleave(function () {
+
+    //        $(this).children('.hide-header-menu').hide();
+    //    });
+
+    //    $('.lbnewspaper').mouseover(function () {
+
+    //        $(this).children('.hide-header-menu').show();
+    //    });
+    //}
+    //showtapchi() {
+
+    //    $('.lbbook').mouseleave(function () {
+
+    //        $(this).children('.hide-header-menu-book').hide();
+    //    });
+
+    //    $('.lbbook').mouseover(function () {
+
+    //        $(this).children('.hide-header-menu-book').show();
+    //    });
+    //}
+
+
+}
+
+var menu = new Menu();
+
+
---
+++ DemoQLTV/MISA.QLTV.VN/MISA.QLTV.VN/scripts/jqueryui/them.js
@@ -0,0 +1,13 @@
+﻿
+$(function () {
+   
+    $("#dialog").dialog({
+        autoOpen: false,
+        width: 340,
+        height:186
+    });
+
+    $("#btnAddCategorySymbol").on("click", function () {
+        $("#dialog").dialog("open");
+    });
+}); 
